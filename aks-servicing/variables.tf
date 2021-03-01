68b62cdee70f145fb4d6c2aaf0955a55884491a2
Vinicius Souza <viniciusabreu743@gmail.com>
2021-03-01 09:34:45 -0300

    Alterando estrutura

--- aks-servicing/variables.tf
+++ aks-servicing/variables.tf
@@ -1,49 +1,71 @@
 variable "appId" {
   description = "Azure Kubernetes Service Cluster service principal"
+  default     = "447347b5-0f22-4b64-aa73-18ea9f408f4d"
 }
 
 variable "password" {
   description = "Azure Kubernetes Service Cluster password"
+  default     = "f511380e-3822-430d-a174-f4a9fe8a8120"
 }
+
+
+# environment   = "development"
+# appId         = "447347b5-0f22-4b64-aa73-18ea9f408f4d"
+# password      = "f511380e-3822-430d-a174-f4a9fe8a8120"
+# resourcename  = "aks-rg"
+# clustername   = "servicing-cluster"
+# location      = "East US"
+# dnspreffix    = "kubecluster-servicing"
+# size          = "Standard_D2_v2"
+# agentnode     = "1"
+# acrname       = "demo01acr"
 
 variable "resourcename" {
     description = "Resource Group name in Azure"
     type        = string
+    default     = "aks-rg"  
 }
 
 
 variable "acrname"{
     description = "Name of Container Register"
     type        = string
+    default     = "demo01acr"
 }
 variable "clustername" {
     description = "Cluster AKS name in Azure"
     type        = string
+    default     = "servicing-cluster"
 }
 
 variable "location" {
     description = "Location of Resource Group"
     type        = string
+    default     = "East US"
 }
 
 variable "dnspreffix" {
     description = "DNS Prefix"
     type        = string
+    default     = "kubecluster-servicing"
 }
 
 variable "size" {
     description = "Size of Cluster in AKS"
     type        = string
+    default     = "Standard_D2_v2"
 }
 
 variable "agentnode" {
     description = "Node Count of Cluster"
-    type        =  string
+    type        = string
+    default     = "1"
 }
 
 variable "environment" {
     description = "Environment name"
     type        = string
+    default     = "development"
 }
 
 # variable "subscription_id" {
