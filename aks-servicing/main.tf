f5b1481c7b62e439f83f78eeb907dcb96ed37b50
Vinicius Souza <viniciusabreu743@gmail.com>
2021-03-01 10:27:48 -0300

    removendo role acr pull

--- aks-servicing/main.tf
+++ aks-servicing/main.tf
@@ -11,13 +11,6 @@ resource "azurerm_resource_group" "default" {
   }
 }
 
-resource "azurerm_role_assignment" "role_acrpull" {
-  scope                            = azurerm_container_registry.acr.id
-  role_definition_name             = "AcrPull"
-  principal_id                     = azurerm_kubernetes_cluster.default.kubelet_identity.0.object_id
-  skip_service_principal_aad_check = true
-}
-
 resource "azurerm_container_registry" "acr" {
   name                = var.acrname
   resource_group_name = azurerm_resource_group.default.name
